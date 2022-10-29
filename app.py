77732e3dcbde6d8a76e155fcea041d107cf7cc3d
Deepak Ravikumar <deepak.r@techmango.net>
2022-10-30 01:59:48 +0530

    removed projects

--- app.py
+++ app.py
@@ -23,10 +23,7 @@ SOCIAL_MEDIA = {
     "GitHub": "https://github.com/WhiteD3vil"
 }
 PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+    "🏆 Yet to add": "https://youtube.com",
 }
 
 
